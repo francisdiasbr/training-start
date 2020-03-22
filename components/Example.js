b885b3119b2e4a523214cd2bb173b8f09b6c8047
francis <francisdiasbr@gmail.com>
2020-03-22 12:09:56 -0300

    terminei a primeira etapa

--- components/Example.js
+++ components/Example.js
@@ -1,1 +1,14 @@
-export default () => <p>Example page</p>
+import './style.css'
+export default () => (
+    <div>
+        <h1>Hello World! </h1>
+        <h2>I announce that I`m here!</h2>
+        <p>hoje é domingo. vamos tentar fazer uma nova linha</p>
+        <p>novo parágrafo </p>
+        <a target="_blank" href="https://www.xprog.com.br">Clique aqui!</a>
+    <img src="https://imagens.canaltech.com.br/empresas/652.400.jpg">
+
+    </img>
+    </div>
+
+)
