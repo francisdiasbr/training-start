28bdf6ca0c24f0d94f760a8a24a2a353bb9429e2
francis <francisdiasbr@gmail.com>
2020-03-22 17:13:49 -0300

    incluido novo texto no paragrafo a fim de treinar o processo de in e out

--- components/Example.js
+++ components/Example.js
@@ -4,7 +4,7 @@ export default () => (
         <h1>Hello World! </h1>
         <h2>I announce that I`m here!</h2>
         <p>here some tips to enjoy your weekend!</p>
-        <p>novo parágrafo </p>
+        <p>novo parágrafo. blablabla </p>
         <a target="_blank" href="https://www.xprog.com.br">Clique aqui!</a>
         <img src="https://www.yesmagazine.org/wp-content/uploads/imports/97c16c71e4ac4236bc5058d63a667663.png"></img>
     </div>
