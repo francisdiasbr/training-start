8b69f1e63c1c73a4a6881a1e8fb72c9129af2089
francis <francisdiasbr@gmail.com>
2020-03-22 12:25:50 -0300

    alteração de conteúdo. sem mudança de estilos

--- components/Example.js
+++ components/Example.js
@@ -1,14 +1,11 @@
 import './style.css'
 export default () => (
-    <div>
+    <div className="corpo">
         <h1>Hello World! </h1>
         <h2>I announce that I`m here!</h2>
-        <p>hoje é domingo. vamos tentar fazer uma nova linha</p>
+        <p>here some tips to enjoy your weekend!</p>
         <p>novo parágrafo </p>
         <a target="_blank" href="https://www.xprog.com.br">Clique aqui!</a>
-    <img src="https://imagens.canaltech.com.br/empresas/652.400.jpg">
-
-    </img>
+        <img src="https://www.yesmagazine.org/wp-content/uploads/imports/97c16c71e4ac4236bc5058d63a667663.png"></img>
     </div>
-
 )
